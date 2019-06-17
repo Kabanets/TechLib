bb5c9a4d1abec728f758daa73b837f67cd698fa0
Kabanets <andrey.kabanets@live.ru>
2019-06-17 10:04:33 +0300

    AuthorRepo

---
+++ TechLib.DataAccess.Tests/Repositories/AuthorRepoTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechLib.DataAccess.Repositories;
+using TechLib.Domain.Entities;
+using TechLib.Domain.Repositories;
+
+namespace TechLib.DataAccess.Tests.Repositories
+{
+    [TestClass]
+    public class AuthorRepoTests
+    {
+        private readonly IAuthorRepo authorRepo;
+        public AuthorRepoTests()
+        {
+            authorRepo = new AuthorRepo(new AppDbContext());
+        }
+
+        [TestMethod]
+        public void CanCreateAuthor()
+        {
+            var author = new Author
+            {
+                FName = "FName1",
+                LName = "LName1"
+            };
+
+            authorRepo.SaveAuthor(author);
+
+            var autorCreated = authorRepo.Authors.AsQueryable().FirstOrDefault(a => a.FName == "FName1" && a.LName == "LName1");
+            Assert.IsNotNull(autorCreated);
+        }
+
+        [TestMethod]
+        public void CanEditAuthor()
+        {
+            var author = authorRepo.Authors.AsQueryable().FirstOrDefault(a => a.FName == "FName1");
+            Assert.IsNotNull(author);
+
+            author.FName = "FName2";
+            author.LName = "LName2";
+
+            authorRepo.SaveAuthor(author);
+
+            var authorEdited = authorRepo.Authors.AsQueryable().FirstOrDefault(a => a.FName == "FName2" && a.LName == "LName2");
+            Assert.IsNotNull(authorEdited);
+        }
+
+        [TestMethod]
+        public void CanDeleteAuthor()
+        {
+            var author = authorRepo.Authors.AsQueryable().FirstOrDefault(a => a.FName == "FName2");
+            Assert.IsNotNull(author);
+
+            authorRepo.DeleteAuthor(author.AuthorId);
+
+            author = authorRepo.Authors.AsQueryable().FirstOrDefault(a => a.FName == "FName2");
+            Assert.IsNull(author);
+        }
+    }
+}
---
+++ TechLib.DataAccess/Repositories/AuthorRepo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using TechLib.Domain.Entities;
+using TechLib.Domain.Repositories;
+
+namespace TechLib.DataAccess.Repositories
+{
+    public class AuthorRepo : IAuthorRepo
+    {
+        private readonly AppDbContext db;
+
+        public AuthorRepo(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<Author> Authors => db.Authors;
+
+        public void SaveAuthor(Author author)
+        {
+            if(author.AuthorId == 0)
+            {
+                db.Authors.Add(author);
+            }
+            else
+            {
+                db.Entry(author).State = EntityState.Modified;
+            }
+
+            db.SaveChanges();
+        }
+
+        public void DeleteAuthor(int authorId)
+        {
+            var author = db.Authors.Find(authorId);
+            if (author == null) return;
+
+            db.Authors.Remove(author);
+            db.SaveChanges();
+        }
+
+
+    }
+}
